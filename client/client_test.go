f6ed8a50ffc60187a865611ef3ee8a622e89c4db
sergei.iurchenko <sergei.iurchenko@check24.de>
2024-03-03 13:58:58 +0100

    feat: more basic tests for client

--- client/client_test.go
+++ client/client_test.go
@@ -11,3 +11,21 @@ func TestNewClient(t *testing.T) {
 		t.Errorf("Expected %s, got %s", apiKey, client.apiKey)
 	}
 }
+
+func TestSetBaseURL(t *testing.T) {
+	baseURL := "https://test.com"
+	client := NewClient("test")
+	client.SetBaseURL(baseURL)
+	if client.baseURL != baseURL {
+		t.Errorf("Expected %s, got %s", baseURL, client.baseURL)
+	}
+}
+
+func TestSetApiHost(t *testing.T) {
+	apiHost := "test.com"
+	client := NewClient("test")
+	client.SetApiHost(apiHost)
+	if client.apiHost != apiHost {
+		t.Errorf("Expected %s, got %s", apiHost, client.apiHost)
+	}
+}
