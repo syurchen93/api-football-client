e669cd2df09d35bdb9e99ab59cae6637c94ea298
sergei.iurchenko <sergei.iurchenko@check24.de>
2024-03-28 19:37:52 +0100

    fix: test for player map parser

--- parser/player_map_parser_test.go
+++ parser/player_map_parser_test.go
@@ -7,51 +7,56 @@ import (
 
 type formationTestCase[]struct {
 		formation string
-		expected  map[string]int
-	}
+		expected  map[string]interface{}
+}
 
 func TestParseFormationStringIntoMap(t *testing.T) {
 	testCases := formationTestCase{
 		{
 			formation: "4-4-2",
-			expected: map[string]int{
+			expected: map[string]interface{}{
 				"D": 4,
 				"M": 4,
 				"F":  2,
+				"original": "4-4-2",
 			},
 		},
 		{
 			formation: "4-3-3",
-			expected: map[string]int{
+			expected: map[string]interface{}{
 				"D": 4,
 				"M": 3,
 				"F":  3,
+				"original": "4-3-3",
 			},
 		},
 		{
 			formation: "4-2-3-1",
-			expected: map[string]int{
+			expected: map[string]interface{}{
 				"D":  4,
 				"DM": 2,
 				"AM": 3,
 				"F":  1,
+				"original": "4-2-3-1",
 			},
 		},
 		{
 			formation: "3-4-3",
-			expected: map[string]int{
+			expected: map[string]interface{} {
 				"D": 3,
 				"M": 4,
 				"F":  3,
+				"original": "3-4-3",
 			},
 		},
 		{
 			formation: "4-4-1-1",
-			expected: map[string]int{
+			expected: map[string]interface{} {
 				"D": 4,
 				"M": 4,
 				"AM":  1,
 				"F":  1,
+				"original": "4-4-1-1",
 			},
 		},
 	}
